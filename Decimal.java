3d3f4b6a961e9838d9505af4e7aad1f290823b9a
subayadhav <suba10499@gmail.com>
2019-02-03 21:08:39 +0530

    Create Decimal.java

---
+++ Decimal.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+  public class Decimal{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      double l=1.626;
+      double b=2.31;
+      System.out.println("LENGTH:"+String.format("%.5f", (l*b)));
+      }
+} 
