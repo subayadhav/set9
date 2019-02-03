0b2974fdcc107d1c60cd0e5c4a854fbdb52a83cc
subayadhav <suba10499@gmail.com>
2019-02-03 21:11:57 +0530

    Create  Asciivalue.java

---
+++ Asciivalue.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+  public class Asciivalue{
+    public static void main(String args[]){
+      Scanner s=new Scanner(System.in);
+      System.out.println("Enter a Charecter:");
+      char in=s.next().charAt(0);
+      int Conv=(int)in;
+      System.out.println("ANS:"+Conv);
+      }
+} 
